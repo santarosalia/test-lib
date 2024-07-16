fdb78532c86834f6ebf727da359011e09ef7bb30
gqob <dhkim@edentns.com>
2024-07-16 15:59:51 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,19 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src')
+      }
+    ]
+  },
   build: {
     lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
-      name: 'NpmTest',
-      fileName: 'npm-test'
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'index',
+      fileName: 'index'
     },
     rollupOptions: {
       external: ['vue'],
