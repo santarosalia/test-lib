34045d48b91b4f0f76a4683532fd982c7dc6f25a
gqob <dhkim@edentns.com>
2024-07-18 01:05:25 +0900

    dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),dts({
+    insertTypesEntry: true,
+    tsconfigPath: 'tsconfig.lib.json'
+  }),],
   resolve: {
     alias: [
       {
