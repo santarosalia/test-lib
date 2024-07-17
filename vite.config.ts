794c1446d18175589bd5399c6bd1f8be7a759343
gqob <dhkim@edentns.com>
2024-07-18 00:17:21 +0900

    a

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig({
     alias: [
       {
         find: '@lib',
+        replacement: resolve(__dirname, './lib')
+      },
+      {
+        find: '@lib/',
         replacement: resolve(__dirname, './lib/')
       },
     ]
