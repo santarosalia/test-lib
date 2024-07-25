2129e7aa90d06a86265931fc682a615d40bba58f
gqob <dhkim@edentns.com>
2024-07-25 13:43:08 +0900

    slide transition

--- lib/Select/Select.vue
+++ lib/Select/Select.vue
@@ -23,63 +23,56 @@ const baseClassName = css({
     '& div:first-child': {
         display: 'flex',
         justifyContent: 'space-between'
-    }
+    },
 });
 const modelValueClassName = css({
     cursor: 'default'
 });
-const optionListClassName = css({
+const optionListContainerCss = css({
     position: 'absolute',
     top: '36px',
     width: '100%',
     left: 0,
-    display: 'flex',
+    overflow: 'hidden',
+    boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.10)',
     boxSizing: 'border-box',
+
+});
+const optionListClassName = css({
+    display: 'flex',
+    backgroundColor: color.white,
     overflowX: 'hidden',
     border: `1px solid ${color.gray400}`,
+    width: '100%',
     borderRadius: '6px',
-    backgroundColor: color.white,
-    boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.10)',
-    // '@keyframes dropdown' : {
-    //     '0%': {
-    //         transform: 'translateY(-100%)',
-    //     },
-    //     '100%': {
-    //         transform: 'translateY(0)',
-    //     },
-    // },
-    animation: '0.5s ease',
-    '& ul': {
-        flexDirection: 'column',
-        alignItems: 'flex-start',
-        flex: '1 0 0',
-        listStyleType: 'none',
-        margin: 0,
-        padding: 0,
-        overflowX: 'hidden',
-        overflowY: 'scroll',
-        maxHeight: '220px',
-        '::-webkit-scrollbar': {
-            width: '4px',
-            height: '4px',
-            backgroundColor: 'gray'
-        },
-        '::-webkit-scrollbar-thumb': {
-            backgroundColor: 'black',
-            borderRadius: '5px'
-        },
-        '& li': {
-            boxSizing: 'border-box',
-            display: 'flex',
-            height: '48px',
-            alignItems: 'center',
-            padding: '12px',
-            ':hover': {
-                backgroundColor: color.blue300
-            }
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    flex: '1 0 0',
+    listStyleType: 'none',
+    margin: 0,
+    padding: 0,
+    overflowY: 'scroll',
+    maxHeight: '220px',
+    '::-webkit-scrollbar': {
+        width: '4px',
+        height: '4px',
+        backgroundColor: 'gray'
+    },
+    '::-webkit-scrollbar-thumb': {
+        backgroundColor: 'black',
+        borderRadius: '5px'
+    },
+    '& li': {
+        width: '100%',
+        boxSizing: 'border-box',
+        display: 'flex',
+        height: '48px',
+        alignItems: 'center',
+        padding: '12px',
+        ':hover': {
+            backgroundColor: color.blue300
         }
     },
-    
 });
 const toggleSelect = (e: MouseEvent) => {
     isOpen.value = !isOpen.value;
@@ -119,6 +112,30 @@ const rotate = css({
     transform: 'rotate(0.5turn)'
 });
 
+const slideDown = css({
+    '@keyframes slideDown': {
+        '0%': {
+            transform: 'translateY(-100%)'
+        },
+        '100%': {
+            transform: 'translateY(0)'
+        }
+    },
+    animation: 'slideDown 0.5s ease'
+});
+
+const slideUp = css({
+    '@keyframes slideUp': {
+        '0%': {
+            transform: 'translateY(0)'
+        },
+        '100%': {
+            transform: 'translateY(-100%)'
+        }
+    },
+    animation: 'slideUp 0.5s ease'
+
+})
 </script>
 <template>
     <div :class="baseClassName">
@@ -128,20 +145,25 @@ const rotate = css({
             </span>
             <Icon icon="arrowBottom" width="24px" height="24px" :class="isOpen ? rotate : ''" color="black"></Icon>
         </div>
-        <div
-            v-if="isOpen"
-            :class="optionListClassName"
-            >
-            <ul>
-                <li
-                    v-for="(option) in optionList"
-                    ref="optionElementList"
-                    :class="isSelected(option) ? selectedClassName : null"
-                    @click="onClickOption"
+        <div :class="optionListContainerCss">
+            <Transition
+                :enter-active-class="slideDown"
+                :leave-active-class="slideUp"
+                >
+                <ul
+                    v-if="isOpen"
+                    :class="optionListClassName"
                     >
-                    {{ option }}
-                </li>
-            </ul>
+                    <li
+                        v-for="(option) in optionList"
+                        ref="optionElementList"
+                        :class="isSelected(option) ? selectedClassName : null"
+                        @click="onClickOption"
+                        >
+                        {{ option }}
+                    </li>
+                </ul>
+            </Transition>
         </div>
     </div>
     
